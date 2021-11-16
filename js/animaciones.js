97c379cefd2e8ba8b69b58dd477b88c9ced86417
Elvis Josué <68576522+iElvisJosue@users.noreply.github.com>
2021-11-16 12:41:26 -0600

    Add files via upload

--- js/animaciones.js
+++ js/animaciones.js
@@ -2,7 +2,10 @@ const MostrarInformacion = document.getElementsByClassName('MostrarInformacion')
 const InformacionCerrar = document.querySelector('.Informacion-Cerrar');
 const VerInformacion = document.querySelector('.VerInformacion');
 
+const NuevoRegistro = document.querySelector('.NuevoRegistro');
+const RegistrarCerrar = document.querySelector('.Registrar-IconoCerrar');
 
+////////////////////////////////////////////////////////////
 for (i = 0; i < MostrarInformacion.length; i++){
     MostrarInformacion[i].addEventListener('click', () => {
         VerInformacion.classList.add('Informacion-Mostrar');
@@ -10,4 +13,12 @@ for (i = 0; i < MostrarInformacion.length; i++){
 }
 InformacionCerrar.addEventListener('click', () => {
     VerInformacion.classList.remove('Informacion-Mostrar');
+})
+
+////////////////////////////////////////////////////////////
+NuevoRegistro.addEventListener('click', () => {
+    RegistrarUsuario.classList.add('RegistrarUsuario-Mostrar');
+})
+RegistrarCerrar.addEventListener('click', () => {
+    RegistrarUsuario.classList.remove('RegistrarUsuario-Mostrar');
 })
