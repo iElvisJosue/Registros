0678a7e857bfb60b60e30fd211cbb22c1910647c
Elvis Josué <68576522+iElvisJosue@users.noreply.github.com>
2021-11-17 13:28:26 -0600

    Add files via upload

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,11 +1,30 @@
+////// OBTENER BOTONES DEL HTML  ///////
+const BtnNuevoRC = document.querySelector('.NuevoRC');
+const BtnNuevoContaco = document.querySelector('.NuevoContacto');
+const BtnNuevaCita = document.querySelector('.NuevaCita');
+
+////// SECCION DE MOSTRAR LA INFORMACION DEL CONTACTO  ///////
 const MostrarInformacion = document.getElementsByClassName('MostrarInformacion');
-const InformacionCerrar = document.querySelector('.Informacion-Cerrar');
 const VerInformacion = document.querySelector('.VerInformacion');
+const InformacionCerrar = document.querySelector('.Informacion-Cerrar');
 
-const NuevoRegistro = document.querySelector('.NuevoRegistro');
-const RegistrarCerrar = document.querySelector('.Registrar-IconoCerrar');
+////// SECCION DE REGISTRAR COACTO   ///////
+const RegistrarContacto = document.querySelector('.RegistrarContacto');
+const RegistrarCerrar = document.querySelector('.Form-IconoCerrar');
 
-////////////////////////////////////////////////////////////
+
+////// SECCION DE REGISTRAR COACTO   ///////
+const RegistrarCita = document.querySelector('.RegistrarCita');
+const CitaCerrar = document.querySelector('.Form-IconoCitaCerrar');
+
+
+////// FUCION PARA MOSTRAR LOS BOTONES DE AGREGAR CONTACTO O USUARIO ///////
+BtnNuevoRC.addEventListener('click', () => {
+    BtnNuevoContaco.classList.toggle('MostrarNuevoContacto');
+    BtnNuevaCita.classList.toggle('MostrarNuevaCita');
+})
+
+////// FUCION PARA MOSTRAR LA SECCION DE MOSTRAR LA INFORMACION DEL CONTACTO  ///////
 for (i = 0; i < MostrarInformacion.length; i++){
     MostrarInformacion[i].addEventListener('click', () => {
         VerInformacion.classList.add('Informacion-Mostrar');
@@ -15,10 +34,19 @@ InformacionCerrar.addEventListener('click', () => {
     VerInformacion.classList.remove('Informacion-Mostrar');
 })
 
-////////////////////////////////////////////////////////////
-NuevoRegistro.addEventListener('click', () => {
-    RegistrarUsuario.classList.add('RegistrarUsuario-Mostrar');
+////// FUCION PARA MOSTRAR LA SECCION DE REGISTRAR CONTACTO  ///////
+BtnNuevoContaco.addEventListener('click', () => {
+    RegistrarContacto.classList.add('RegistrarContacto-Mostrar');
 })
 RegistrarCerrar.addEventListener('click', () => {
-    RegistrarUsuario.classList.remove('RegistrarUsuario-Mostrar');
+    RegistrarContacto.classList.remove('RegistrarContacto-Mostrar');
+})
+
+
+////// FUCION PARA MOSTRAR LA SECCION DE REGISTRAR CONTACTO  ///////
+BtnNuevaCita.addEventListener('click', () => {
+    RegistrarCita.classList.add('RegistrarCita-Mostrar');
+})
+CitaCerrar.addEventListener('click', () => {
+    RegistrarCita.classList.remove('RegistrarCita-Mostrar');
 })
